8451eac0792809d6dc1a1d9a5825d0484d778fd3
stanley <panteleevstas@gmail.com>
2023-08-23 09:59:01 +0500

    majority-element

---
+++ Array/majority-element.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {                
+        int majority = nums[0];
+        int count = 0;
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (count == 0)
+               majority = nums[i];                     
+            
+            if (majority == nums[i])
+              count++;
+            else count--; 
+        }
+
+        return majority;        
+    }
+}
