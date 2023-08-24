7681efed638af22b3717d4654bba86e58ee1cd4a
stanley <panteleevstas@gmail.com>
2023-08-24 08:52:36 +0500

    intersection-of-two-arrays

---
+++ Array/intersection-of-two-arrays.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+
+       var set = new HashSet<int>();
+       for (int i = 0; i < nums1.Length; i++) {
+         set.Add(nums1[i]); 
+       }
+
+       var resSet = new HashSet<int>();
+       for (int i = 0; i < nums2.Length; i++) {
+         if (set.Contains(nums2[i]))
+         resSet.Add(nums2[i]);
+       }
+
+      return resSet.ToArray();                
+    }
+}
